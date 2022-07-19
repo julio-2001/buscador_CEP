2be3f141439441c0303b77ad593df7607552b0c8
julio gabriel <santos2001martins@gmail.com>
2022-07-19 12:20:37 -0300

    :FEAT: Adicionado components Adicionado components: TItleBC, InputBC, e DivBC

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,39 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from 'react';
+import {MainBC , TitleBC,InputBC ,DivBC}from './components/Main';
+
+//Icone da lupa
+import {BsSearch} from "react-icons/bs"
+
 
 function App() {
+  const [input, setInput] = useState("")
   return (
 
     <div className="App">
+      
+      <MainBC>
 
-      <App/>
+        <TitleBC>
+          Address Finder
+        </TitleBC>
 
+    
+        <DivBC>
+  
+          <InputBC
+            placeholder="Digite um CEP"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+          />
+
+          
+          <BsSearch
+          size={25}/>
+
+        </DivBC>
+
+
+      </MainBC>
     </div>
   )
 } 
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,2 +1,46 @@
 import React from "react";
+import Styled from "styled-components"
 
+//Corpo principal da pagina
+const MainBC = Styled.main`
+
+    width:100%;
+    height:100vh;
+    background:black;
+    display:flex;
+    align-items:center;
+    justify-content:center;
+    flex-direction:column;
+`;
+
+//texto principal da pagina
+const TitleBC = Styled.h1`
+    font-size:72px;
+    font-weight:700;
+    color:white;
+    display:flex;
+
+    //TODO colocar animação de flutuação
+`;
+
+// o input 
+const InputBC = Styled.input`
+
+    width:50%;
+    min-height:50px;
+    
+
+`;
+
+//Aqui são as divições
+const DivBC = Styled.div`
+    width:20%;
+    height:50px;
+    background:red;
+   
+
+
+`;
+
+
+export {MainBC ,TitleBC, InputBC,DivBC};
